d243d1d57daa14e119ce8abeb0dd00f80659041e
Pouria-GH <pouria.ghafarzadeh@gmail.com>
2025-11-09 08:17:40 +0330

    Initialize client app

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,14 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 using var scope = app.Services.CreateScope();
@@ -29,6 +37,8 @@ catch (Exception ex)
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An Error occured during migration");
 }
+
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
