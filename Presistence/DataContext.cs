c214e0be9f44becb0903b162a44a9a265818e494
Pouria7gh <pouria.ghafarzadeh@gmail.com>
2025-12-04 20:34:25 +0330

    feat(domain): add activity attendee join table and migration.

--- Presistence/DataContext.cs
+++ Presistence/DataContext.cs
@@ -11,5 +11,18 @@ namespace Presistence
         }
 
         public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ActivityAttendee>(entity =>
+            {
+                entity.HasKey(e => new { e.ActivityId, e.AppUserId });
+                entity.HasOne(e => e.Activity).WithMany(e => e.Attendees).HasForeignKey(e => e.ActivityId);
+                entity.HasOne(e => e.AppUser).WithMany(e => e.Activities).HasForeignKey(e => e.AppUserId);
+            });
+        }
     }
 }
