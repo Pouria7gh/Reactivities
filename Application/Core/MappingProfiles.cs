fbc41a63321f569a4619258f4b8c5e33d54c8db8
pouria7gh <pouria.ghafarzadeh@gmail.com>
2026-02-06 13:45:02 +0330

    feat(application): add follow feature properties to profile, attendee dto and configure mapping profile for them.

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,5 +1,6 @@
 using Application.Activities;
 using Application.Comments;
+using Application.Interfaces;
 using AutoMapper;
 using Domain;
 
@@ -7,8 +8,11 @@ namespace Application.Core;
 
 public class MappingProfiles : Profile
 {
-    public MappingProfiles()
+    private readonly IUserAccessor _userAccessor;
+    public MappingProfiles(IUserAccessor userAccessor)
     {
+        _userAccessor = userAccessor;
+
         CreateMap<Activity, Activity>();
         
         CreateMap<Activity, ActivityDto>()
@@ -17,14 +21,21 @@ public class MappingProfiles : Profile
         
         CreateMap<AppUser, Profiles.Profile>()
             .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.Where(x => x.IsMain)
-                .Select(x => x.Url).FirstOrDefault()));
+                .Select(x => x.Url).FirstOrDefault()))
+            .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
+            .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count))
+            .ForMember(d => d.Following, o => o.MapFrom(s =>
+                s.Followers.Any(x => x.Observer.UserName == _userAccessor.GetUserName())));
         
         CreateMap<ActivityAttendee, AttendeeDto>()
             .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
             .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
             .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos
-                .Where(p => p.IsMain).Select(x => x.Url).FirstOrDefault()));
+                .Where(p => p.IsMain).Select(x => x.Url).FirstOrDefault()))
+            .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.AppUser.Followers.Count))
+            .ForMember(d => d.Following, o => o.MapFrom(s => 
+                s.AppUser.Followers.Any(x => x.Observer.UserName == _userAccessor.GetUserName())));
 
         CreateMap<Comment, CommentDto>()
             .ForMember(c => c.Username, o => o.MapFrom(s => s.Author.UserName))
