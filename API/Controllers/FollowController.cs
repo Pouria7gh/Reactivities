b7145f45f4868ec32c11d099dec6e62d13ebf7c9
pouria7gh <pouria.ghafarzadeh@gmail.com>
2026-02-06 13:41:24 +0330

    feat(api): add list following and followers endpoint;

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -11,5 +11,19 @@ namespace API.Controllers
             var result = await Send(new FollowToggle.Command() {TargetUsername = targetUsername});
             return HandleResult(result);
         }
+
+        [HttpGet("{username}/Followers")]
+        public async Task<IActionResult> GetFollowers(string username)
+        {
+            var followers = await Send(new ListFollowers.Query() {Username = username});
+            return HandleResult(followers);
+        }
+
+        [HttpGet("{username}/Followings")]
+        public async Task<IActionResult> GetFollowings(string username)
+        {
+            var followings = await Send(new ListFollowings.Query() {Username = username});
+            return HandleResult(followings);
+        }
     }
 }
