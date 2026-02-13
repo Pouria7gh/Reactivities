c1c0d3eb2cf972b2d7682b31a72b1b84a5d4e74c
pouria7gh <pouria.ghafarzadeh@gmail.com>
2026-02-13 12:56:21 +0330

    feat(application): add pagination.

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,18 +1,19 @@
 using Application.Core;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
-using Domain;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Presistence;
 
 namespace Application.Activities;
 
 public class List
 {
-    public class Query : IRequest<Result<List<ActivityDto>>> { }
+    public class Query : IRequest<Result<PagedList<ActivityDto>>>
+    {
+        public PagingParams Params { get; set; }
+    }
 
-    public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
+    public class Handler : IRequestHandler<Query, Result<PagedList<ActivityDto>>>
     {
         private readonly DataContext _dataContext;
         private readonly IMapper _mapper;
@@ -22,13 +23,16 @@ public class List
             _mapper = mapper;
         }
 
-        public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
+        public async Task<Result<PagedList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
         {
-            var activities = await _dataContext.Activities
+            var query = _dataContext.Activities
+                .OrderByDescending(d => d.Date)
                 .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
-                .ToListAsync(cancellationToken);
+                .AsQueryable();
 
-            return Result<List<ActivityDto>>.Success(activities);
+            var activities = await PagedList<ActivityDto>.CreateAsync(query, request.Params.pageNumber, request.Params.PageSize);
+
+            return Result<PagedList<ActivityDto>>.Success(activities);
         }
     }
 }
